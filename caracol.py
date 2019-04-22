a80f3542a1f6a6d970dcd35839183c5e26020080
nicolaslopez99 <42530363+nicolaslopez99@users.noreply.github.com>
2019-04-21 20:17:21 -0500

    Update caracol.py

--- caracol.py
+++ caracol.py
@@ -4,22 +4,48 @@ Editor de Spyder
 
 Este es un archivo temporal.
 """
-lista=[]
+
+
+def ordenar (m,lista): 
+    if m == [['10']]:
+        #print("la matriz esta vacia")
+        
+        return lista.append('10')
+    else:
+        lista.append(','.join(m[0]))
+        m.remove(m[0])
+        return  ordenar(voltear(m),lista),lista 
+    
+
 def voltear (m):
     filas = len(m)
-    col = len(m[0])
-    return [[m[i][j] for i in range(0, filas, 1)] for j in range(col-1, -1, -1)]
-def ordenar (m,lista): 
-    if not m:
-        print("la matriz esta vacia")
-        return 0
-    lista = lista + m[0]
-    m.remove(m[0])
-    print(lista)
-    m=voltear(m)
-    return  ordenar(m,lista),lista
+    col = len(m[0])-1
+    return [[m[i][j] for i in range(0, filas, 1)] for j in range(col,-1 , -1)]
 
 m1=[x.split()for x in open ("C:\\Users\\Antonio\\matriz.txt").readlines()]
+lista=[]
+#print(ordenar(m1,lista))
+a= ','.join(ordenar(m1,lista))
+print(a)
 
-ordenar(m1,lista)
 
+#
+#def ordenar (m,lista): 
+##    if m == [['10']]:
+ #       #print("la matriz esta vacia")
+ #       lista.append('10')
+ #       return 0
+ #   else:
+ #       lista.append(','.join(m[0]))
+ #       m.remove(m[0])
+#        return  ordenar(voltear(m),lista),lista 
+    
+
+##def voltear (m):
+ #   filas = len(m)
+ #   col = len(m[0])-1
+ #   return [[m[i][j] for i in range(0, filas, 1)] for j in range(col,-1 , -1)]
+
+#m1=[x.split()for x in open ("C:\\Users\\Antonio\\matriz.txt").readlines()]
+#lista=[]
+#print(ordenar(m1,lista))
